b0dab31ed91c46498681d6f8060cece467f758de
Taylor Beebe <tabeebe@microsoft.com>
2022-12-01 09:45:03 -0800

    add translation table parse

--- ArmPkg/Include/Library/ArmMmuLib.h
+++ ArmPkg/Include/Library/ArmMmuLib.h
@@ -13,6 +13,37 @@
 
 #include <Library/ArmLib.h>
 
+typedef union {
+  struct {
+    UINT64    Valid                      : 1;
+    UINT64    BlockOrTable               : 1;
+    UINT64    AttributeIndex             : 3;
+    UINT64    NonSecure                  : 1;
+    UINT64    AccessPermissions          : 2;
+    UINT64    Sharability                : 2;
+    UINT64    AccessFlag                 : 1;
+    UINT64    NonGlobal                  : 1;
+    UINT64    Oa                         : 4;
+    UINT64    Nt                         : 1;
+    UINT64    OutputAddress              : 33;
+    UINT64    Guarded                    : 1;
+    UINT64    Dirty                      : 1;
+    UINT64    Contiguous                 : 1;
+    UINT64    PXn                        : 1;
+    UINT64    Xn                         : 1;
+    UINT64    Ignored                    : 4;
+    UINT64    PageBasedHardwareAttribute : 4;
+    UINT64    Reserved                   : 1;
+  } Bits;
+  UINT64    Uint64;
+} TRANSLATION_TABLE_ATTRIBUTE;
+
+typedef struct {
+  UINT64                         LinearAddress;
+  UINT64                         Length;
+  TRANSLATION_TABLE_ATTRIBUTE    Attribute;
+} TRANSLATION_TABLE_ENTRY;
+
 EFI_STATUS
 EFIAPI
 ArmConfigureMmu (
@@ -62,6 +93,16 @@ ArmSetMemoryAttributes (
   IN EFI_PHYSICAL_ADDRESS  BaseAddress,
   IN UINT64                Length,
   IN UINT64                Attributes
+  );
+
+/**
+  BEEBE TODO
+**/
+EFI_STATUS
+EFIAPI
+TranslationTableParse (
+  IN     TRANSLATION_TABLE_ENTRY  *Map,
+  IN OUT UINTN                    *MapCount
   );
 
 #endif // ARM_MMU_LIB_H_
